0ace73e682d9d7ec814a51a253037d7f954ac98b
Seyed Younes Mirmohammadi <mir.younes01@gmail.com>
2021-03-05 17:51:18 +0330

    accounts app created

--- Blog/urls.py
+++ Blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor',include('ckeditor_uploader.urls')),
     path('',include('Post.urls',namespace="Post")),
+    path('accounts/',include('accounts.urls',namespace="accounts")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
